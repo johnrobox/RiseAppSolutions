3a4ffffe4bc1fa882674d20d8f1e766bf694192d
johnrobox <johnrobertjerodiaz@gmail.com>
2016-12-15 15:54:03 +0800

    ADD : comments and break lines

--- application/models/Inquiry.php
+++ application/models/Inquiry.php
@@ -1,27 +1,44 @@
 <?php
 /*
-* Blog
+* Inquiry
 * Database Table : inquiries
 */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inquiry extends CI_Model {
 
+    // construct properties
     public function __construct() {
         parent::__construct();
         $this->table = 'inquiries';
     }
 
+    /**
+     * Get all inquiries list
+     * @return object $result
+     */
     public function getAllInquiries() {
         $inqieries = $this->db->get($this->table);
-        return $inqieries->result();
+        $result = $inqieries->result();
+        return $result;
     }
     
+    /**
+     * Delete inquiry by id
+     * @param int $id
+     * @return boolean $result 
+     */
     public function deleteById($id) {
         $this->db->delete($this->table, array('id' => $id));
-        return ($this->db->affected_rows() > 0 ) ? true : false;
+        $result = ($this->db->affected_rows() > 0 ) ? true : false;
+        return $result;
     }
     
+    /**
+     * Get inquiry by id
+     * @param int $id
+     * @return array $result
+     */
     public function getInquiryById($id) {
         $this->db->where('id', $id);
         $query = $this->db->get($this->table);
@@ -30,24 +47,43 @@ class Inquiry extends CI_Model {
         return $result;
     }
     
+    /**
+     * Find Previous and Next by Id
+     * @param int $id
+     * @param string $states
+     * @param string $operator
+     * @return array $result
+     */
     public function findPreviousNextById($id, $states, $operator) {
-       // $states = "max";
         $query = $this->db->query("select * from ".$this->table." where id = (select $states(id) from ".$this->table." where id ".$operator." ".$id.")");
         $result['select'] = ($this->db->affected_rows() > 0 ) ? true : false;
         $result['inquiry'] = $query->result();
         return $result;
     }
     
+    /**
+     * Get First / Last id
+     * @param string $states
+     * @return int $id
+     */
     public function getFirstLastId($states){
         $query = $this->db->query("SELECT id FROM ".$this->table." WHERE id=(SELECT ".$states."(id) FROM ".$this->table.")");
         $row = $query->row();
-        return $row->id;
+        $id = $row->id;
+        return $id;
     }
     
+    /**
+     * Change Status by id
+     * @param int $id
+     * @param int $status
+     * @return boolean $result
+     */
     public function changeStatusById($id, $status) {
         $this->db->where('id', $id);
         $this->db->update($this->table, array('status' => $status));
-        return ($this->db->affected_rows()) ? true : false;
+        $result = ($this->db->affected_rows()) ? true : false;
+        return $result;
     }
         
 }
--- application/views/admin/content/Inquiry/content.php
+++ application/views/admin/content/Inquiry/content.php
@@ -1,19 +1,23 @@
 
-
-
 <?php echo $this->session->flashdata('success'); ?>
 <?php echo $this->session->flashdata('error'); ?>
-
 <div class="alert alert-success alert-dismissible" role="alert" id="successAlert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close"> 
-        <span aria-hidden="true">&times;</span>
-    </button>
+    <?php
+    // alert close button
+    $close_alert = array(
+        'type' => 'button',
+        'class' => 'close',
+        'data-dismiss' => 'alert',
+        'aria-label' => 'Close',
+        'content' => '<span aria-hidden="true">&times;</span>'
+    );
+    echo form_button($close_alert);
+    ?>
 </div>
-
 <div class="inquiryCommonError text-error"></div>
 <?php if ($inquiries) { ?>
 <div class="dataTable_wrapper">
-    <table class="table table-hovered table-bordered" id="stagit-datatable">
+    <table class="table table-hovered table-bordered" id="inquiry-datatable">
         <thead>
             <tr>
                 <th>Firstname</th>
@@ -25,32 +29,56 @@
             </tr>
         </thead>
         <tbody>
-        <?php foreach ($inquiries as $inquiry) { ?>
-        <?php $id = $inquiry->id; ?>
-        <tr class="<?php echo ($inquiry->status) ? 'white-bg' : 'eee-bg';?>" id="inquiryTr<?php echo $id; ?>">
-            <td><?php echo ucfirst(ucwords($inquiry->inquiry_firstname));?></td>
-            <td><?php echo ucfirst(ucwords($inquiry->inquiry_lastname));?></td>
-            <td><?php echo $inquiry->inquiry_email;?></td>
-            <td>
-                <?php $content = $inquiry->inquiry_content;?>
-                <?php echo strlen($content) > 50 ? substr($content,0,50)."..." : $content; ?>
-            </td>
-            <td><?php echo $inquiry->inquiry_date_submitted;?></td>
-            <td>
-                <button class="btn btn-danger btn-xs deleteInquiryButton btn-fixed-one" value="<?php echo $id; ?>">
-                    <span class="glyphicon glyphicon-trash"></span> Delete
-                </button>
-                <button class="btn btn-primary btn-xs showInquiryButton btn-fixed-one" value="<?php echo $id; ?>">Show</button>
-                <br>
-                <button class="btn btn-xs <?php echo ($inquiry->status) ? "btn-success" : " btn-warning";?> btn-fixed-one markInquiryButton " id="markInquiry<?php echo $id;?>" status="<?php echo $inquiry->status;?>" value="<?php echo $id; ?>">
-                    <span id="markText<?php echo $id;?>">
-                        <?php echo ($inquiry->status) ? "Mark as Unread" : " Mark as Read";?>
-                    </span>
-                    <img src="<?php echo base_url();?>images/admin/loading/loading8.gif" id="markLoadingImage<?php echo $id;?>" class="img-responsive loading-image center-block" style="height: 20px; width: 20px; display: none;"/>
-                </button>
-                <button class="btn btn-xs btn-default btn-fixed-one replyInquiryButton" email="<?php echo $inquiry->inquiry_email;?>">Quick Reply</button>
-            </td>
-        </tr>
+        <?php 
+        foreach ($inquiries as $inquiry) { 
+            $id = $inquiry->id; 
+            $status = $inquiry->status;
+            $content = $inquiry->inquiry_content;
+            $email = $inquiry->inquiry_email;
+        ?>
+            <tr class="<?php echo ($status) ? 'white-bg' : 'eee-bg';?>" id="inquiryTr<?php echo $id; ?>">
+                <td><?php echo ucfirst(ucwords($inquiry->inquiry_firstname));?></td>
+                <td><?php echo ucfirst(ucwords($inquiry->inquiry_lastname));?></td>
+                <td><?php echo $email;?></td>
+                <td>
+                    <?php echo strlen($content) > 50 ? substr($content,0,50)."..." : $content; ?>
+                </td>
+                <td><?php echo $inquiry->inquiry_date_submitted;?></td>
+                <td>
+                    <?php 
+                    // delete inquiry button
+                    $delete_inquiry_button = array(
+                        'class' => 'btn btn-danger btn-xs deleteInquiryButton btn-fixed-one',
+                        'value' => $id,
+                        'content' => '<span class="glyphicon glyphicon-trash"></span> Delete'
+                    );
+                    echo form_button($delete_inquiry_button);
+                    // show single inquiry button
+                    $show_inquiry_button = array(
+                        'class' => 'btn btn-primary btn-xs showInquiryButton btn-fixed-one',
+                        'value' => $id,
+                        'content' => 'Show'
+                    );
+                    echo form_button($show_inquiry_button);
+                    ?>
+                    <br>
+                    <button class="btn btn-xs <?php echo ($status) ? "btn-success" : " btn-warning";?> btn-fixed-one markInquiryButton " id="markInquiry<?php echo $id;?>" status="<?php echo $status;?>" value="<?php echo $id; ?>">
+                        <span id="markText<?php echo $id;?>">
+                            <?php echo ($status) ? "Mark as Unread" : " Mark as Read";?>
+                        </span>
+                        <img src="<?php echo base_url();?>images/admin/loading/loading8.gif" id="markLoadingImage<?php echo $id;?>" class="img-responsive loading-image center-block" style="height: 20px; width: 20px; display: none;"/>
+                    </button>
+                    <?php 
+                    // reply inquiry button
+                    $quick_reply_inquiry_button = array(
+                        'class' => 'btn btn-xs btn-default btn-fixed-one replyInquiryButton',
+                        'email' => $email,
+                        'content' => 'Quick Reply'
+                    );
+                    echo form_button($quick_reply_inquiry_button);
+                    ?>
+                </td>
+            </tr>
         <?php } ?>
         </tbody>
     </table>
@@ -61,7 +89,7 @@
 
 <script>
     $(document).ready(function(){
-        $('#stagit-datatable').DataTable({
+        $('#inquiry-datatable').DataTable({
             responsive: true
         });
     });
