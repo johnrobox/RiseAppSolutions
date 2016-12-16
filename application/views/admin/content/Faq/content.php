120d207f73f5b574db8ebb7cb5c320c4eef8024f
johnrobox <johnrobertjerodiaz@gmail.com>
2016-12-16 13:30:46 +0800

    ADD : comment and use ci form helper

--- application/views/admin/content/Faq/content.php
+++ application/views/admin/content/Faq/content.php
@@ -1,7 +1,13 @@
 
-
-<?php echo $this->session->flashdata('success'); ?>
-<button class="btn btn-primary" id="addFaqButton">ADD</button>
+<?php
+echo $this->session->flashdata('success'); 
+$add_button = array(
+    'class' => 'btn btn-primary',
+    'id' => 'addFaqButton',
+    'content' => 'ADD'
+);
+echo form_button($add_button);
+?>
 <div class="alert" id="faqMessageAlert"></div>
 <?php if ($faqs) { ?>
 <div class="dataTable_wrapper">
@@ -15,18 +21,52 @@
         </thead>
         <tbody>
         <?php 
+        foreach ($faqs as $faq) { 
+            $status = $faq->status;
+            if ($status) { 
                 $button_class = 'warning';
                 $button_text = 'Disable';
             } else {
                 $button_class = 'default';
                 $button_text = 'Enable';
             }
+            $id = $faq->id; 
+        ?>
+        <tr class="<?php echo ($status) ? 'white-bg' : 'eee-bg';?>" id="faqTr<?php echo $id; ?>">
             <td><?php echo $faq->faq_question;?></td>
             <td><?php echo $faq->faq_answer;?></td>
             <td>
+                <?php
+                // show button
+                $show_faq_button = array(
+                    'class' => "btn btn-primary btn-xs showFaqInfo",
+                    'value' => $id,
+                    'content' => 'Show'
+                );
+                echo form_button($show_faq_button);
+                
+                // delete button
+                $delete_faq_button = array(
+                    'class' => 'btn btn-danger btn-xs deleteFaqButton',
+                    'value' => $id,
+                    'content' => 'Delete'
+                );
+                echo form_button($delete_faq_button);
+                
+                ?>
+                <button class="btn btn-<?php echo $button_class; ?> btn-xs changeStatusButton" id="changeStatusButton<?php echo $id;?>" value="<?php echo $id;?>" status="<?php echo $status;?>">
                     <span id="text<?php echo $id;?>"><?php echo $button_text; ?></span>
                     <img src="<?php echo base_url();?>images/admin/loading/loading8.gif" id="changeStatusLoading<?php echo $id;?>" class="img-responsive center-block" style="height: 20px; width: 20px; display: none;"/>
                 </button>
+                <?php
+                // update button
+                $update_faq_button = array(
+                    'class' => 'btn btn-success btn-xs updateFaqButton',
+                    'value' => $id,
+                    'content' => 'Update'
+                );
+                echo form_button($update_faq_button);
+                ?>
             </td>
         </tr>
         <?php } ?>
